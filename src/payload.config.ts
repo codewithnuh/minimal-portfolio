e40e04a6f4b3bbd58c9b08f0c660f1b81dca52c5
codewithnuh <nuh25792@gmail.com>
2025-09-19 22:38:12 +0500

    feat: uploadthing storage adapter added

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,6 +2,7 @@
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { uploadthingStorage } from "@payloadcms/storage-uploadthing";
 import path from "path";
 import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
@@ -42,6 +43,15 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    uploadthingStorage({
+      collections: {
+        media: true,
+      },
+      options: {
+        token: process.env.UPLOADTHING_TOKEN,
+        acl: "public-read",
+      },
+    }),
     // storage-adapter-placeholder
   ],
   // email: nodemailerAdapter({
