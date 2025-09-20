39aea579c1c7e7351468c8b58887c99d685b6aed
codewithnuh <nuh25792@gmail.com>
2025-09-20 18:34:58 +0500

    fix: add migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250919_165800_init from './20250919_165800_init';
+import * as migration_20250920_133340 from './20250920_133340';
 
 export const migrations = [
   {
     up: migration_20250919_165800_init.up,
     down: migration_20250919_165800_init.down,
-    name: '20250919_165800_init'
+    name: '20250919_165800_init',
+  },
+  {
+    up: migration_20250920_133340.up,
+    down: migration_20250920_133340.down,
+    name: '20250920_133340'
   },
 ];
